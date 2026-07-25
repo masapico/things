aedb70214c8834845ef7d0b329da2608ec09f4fe
Masahiro Arakane <masapico8@gmail.com>
2026-07-25 21:48:41 +0900

    autocancelをfalseに

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -2,6 +2,9 @@ import PocketBase from "pocketbase";
 
 export const pb = new PocketBase("http://127.0.0.1:8090");
 
+// 同じコレクションへの連続リクエストで先行リクエストがキャンセルされるのを防ぐ
+pb.autoCancellation(false);
+
 export function Logout() {
   pb.authStore.clear();
 }
